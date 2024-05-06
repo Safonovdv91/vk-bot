78d5d4496cf89d3dddda133cd4b2668aee745e26
Safonov <Sofik.d@gmail.com>
2024-05-06 09:48:38 +0300

    Переделал метод добавление админа через upsert

--- app/store/admin/accessor.py
+++ app/store/admin/accessor.py
@@ -2,6 +2,7 @@ from hashlib import sha256
 from typing import TYPE_CHECKING
 
 from sqlalchemy import select
+from sqlalchemy.dialects.postgresql import insert
 
 from app.admin.models import AdminModel
 from app.base.base_accessor import BaseAccessor
@@ -13,7 +14,7 @@ if TYPE_CHECKING:
 class AdminAccessor(BaseAccessor):
     async def connect(self, app: "Application") -> None:
         self.logger.info("Инициализируем начального юзера")
-        await self.create_admin(
+        await self.upsert_admin(
             email=app.config.admin.email, password=app.config.admin.password
         )
 
@@ -23,18 +24,18 @@ class AdminAccessor(BaseAccessor):
             query = select(AdminModel).where(AdminModel.email == email)
             return await session.scalar(query)
 
-    async def create_admin(self, email: str, password: str) -> AdminModel:
+    async def upsert_admin(self, email: str, password: str) -> AdminModel:
         self.logger.info("Создаем админа")
-        admin = AdminModel(
-            email=email, password=sha256(password.encode()).hexdigest()
-        )
+        admin_data = {
+            "email": email,
+            "password": sha256(password.encode()).hexdigest(),
+        }
+
+        stmt = insert(AdminModel).values(admin_data)
+        stmt = stmt.on_conflict_do_nothing(index_elements=["email"])
+
         async with self.app.database.session() as session:
-            exist_admin = await self.get_by_email(admin.email)
-            if exist_admin is None:
-                self.logger.info("Добавляем нового администратора")
-                session.add(admin)
-                await session.commit()
-            else:
-                self.logger.warning("Пользователь уже существует")
-                return admin
-        return admin
+            await session.execute(stmt)
+            await session.commit()
+
+            return await self.get_by_email(email)
