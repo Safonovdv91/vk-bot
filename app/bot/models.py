a18fec9a69b84cbafaed576651e255cc632bf0e1
Safonov <Sofik.d@gmail.com>
2024-05-09 10:46:16 +0300

    Добавил модели игры с разработанной схемой

--- app/bot/models.py
+++ app/bot/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.quize.models import QuestionModel
+from app.quize.models import AnswerModel, QuestionModel
 from app.store.database import BaseModel
 
 
@@ -12,12 +12,17 @@ class GameModel(BaseModel):
     pinned_conversation_message_id: Mapped[int | None] = mapped_column(
         default=None
     )
-
     question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
-    question: Mapped["QuestionModel"] = relationship(back_populates="answers")
-
     stage_id: Mapped[int] = mapped_column(ForeignKey("stages.id"))
-    stages: Mapped["StageModel"] = relationship(back_populates="stages")
+
+    stage: Mapped["StageModel"] = relationship(back_populates="games")
+    question: Mapped["QuestionModel"] = relationship(back_populates="games")
+    players: Mapped[list["PlayerModel"]] = relationship(
+        back_populates="game"
+    )
+    player_answers_games: Mapped[list["PlayerAnswerGameModel"]] = relationship(
+        back_populates="game", cascade="all, delete-orphan"
+    )
 
 
 class StageModel(BaseModel):
@@ -25,6 +30,36 @@ class StageModel(BaseModel):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str | None] = mapped_column(String[40])
 
-    games: Mapped[list["GameModel"]] = relationship(
-        relationship(back_populates="games")
+    games: Mapped[list["GameModel"]] = relationship(back_populates="stage")
+
+
+class PlayerModel(BaseModel):
+    __tablename__ = "players"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    vk_user_id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    name: Mapped[str] = mapped_column(String[50])
+    game_id: Mapped[int] = mapped_column(ForeignKey("games.id"))
+
+    game: Mapped["GameModel"] = relationship(back_populates="players")
+    player_answers_games: Mapped[list["PlayerAnswerGameModel"]] = relationship(
+        back_populates="player", cascade="all, delete-orphan"
+    )
+
+
+class PlayerAnswerGameModel(BaseModel):
+    __tablename__ = "player_answers_games"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    player_id: Mapped[int] = mapped_column(ForeignKey("players.id"))
+    game_id: Mapped[int] = mapped_column(ForeignKey("games.id"))
+    answer_id: Mapped[int] = mapped_column(ForeignKey("answers.id"))
+
+    player: Mapped["PlayerModel"] = relationship(
+        back_populates="player_answers_games"
+    )
+    game: Mapped["GameModel"] = relationship(
+        back_populates="player_answers_games"
+    )
+    answer: Mapped["AnswerModel"] = relationship(
+        back_populates="player_answers_games"
     )
--- app/quize/models.py
+++ app/quize/models.py
@@ -1,7 +1,12 @@
+import typing
+
 from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.store.database.sqlalchemy_base import BaseModel
+
+if typing.TYPE_CHECKING:
+    from app.bot.models import PlayerAnswerGameModel, GameModel
 
 
 class ThemeModel(BaseModel):
@@ -27,12 +32,19 @@ class QuestionModel(BaseModel):
         back_populates="question", cascade="all, delete-orphan"
     )
 
+    games: Mapped[list["GameModel"]] = relationship(
+        back_populates="question"
+    )
+
 
 class AnswerModel(BaseModel):
     __tablename__ = "answers"
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String[50], unique=True)
     score: Mapped[int] = mapped_column(default=1, nullable=False)
-
     question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
+
     question: Mapped["QuestionModel"] = relationship(back_populates="answers")
+    player_answers_games: Mapped[list["PlayerAnswerGameModel"]] = relationship(
+        back_populates="answer", cascade="all, delete-orphan"
+    )
