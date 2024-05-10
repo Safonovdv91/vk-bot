c0f09adff0799e6ca40a813755d064aa0797ffbb
Dmitry <42798721+Safonovdv91@users.noreply.github.com>
2024-05-10 19:31:47 +0300

    Добавил модели SQLAlchemy (#4)
    * Добавил модели вопросов - ответов
    
    * Добавил модели игра, этапы игры
    
    * Добавил алембик в гит
    
    * Добавил модели игры с разработанной схемой
    
    * Убрал уникальность vkid у игрока.
    
    * Убрам Model в названии, добавил ENUM
    
    * Убрам Model в названии, добавил ENUM
    
    * Добавил вручную ENUM в миграции, после даунгрейда ругается что уже существует
    
    * refactoring
    
    * убрал  game_stage.create(op.get_bind())
    
    * добавил default_server
    
    * Импортировал Enum из диалектов
    
    * refactoring
    
    * refactoring
    
    * refactoring
    
    ---------
    
    Co-authored-by: Safonov <Sofik.d@gmail.com>

---
+++ app/game/models.py
@@ -0,0 +1,66 @@
+from enum import Enum
+
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.dialects.postgresql import ENUM as PG_ENUM
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.quize.models import Answer, Question
+from app.store.database import BaseModel
+
+
+class GameStage(Enum):
+    NOT_START = "NOT_START"
+    REGISTRATION_GAMERS = "REGISTRATION_GAMERS"
+    WAITING_CALLBACK = "WAITING_CALLBACK"
+    WAITING_ANSWER = "WAITING_ANSWER"
+    FINISHED = "FINISHED"
+    CANCELED = "CANCELED"
+
+
+class Game(BaseModel):
+    __tablename__ = "games"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    conversation_id: Mapped[int | None] = mapped_column(default=None)
+    pinned_conversation_message_id: Mapped[int | None] = mapped_column(
+        default=None
+    )
+    question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
+    game_stage: Mapped[PG_ENUM] = mapped_column(
+        PG_ENUM(GameStage), default=GameStage.NOT_START.value, nullable=False
+    )
+
+    question: Mapped["Question"] = relationship(back_populates="games")
+    players: Mapped[list["Player"]] = relationship(back_populates="game")
+    player_answers_games: Mapped[list["PlayerAnswerGame"]] = relationship(
+        back_populates="game", cascade="all, delete-orphan"
+    )
+
+
+class Player(BaseModel):
+    __tablename__ = "players"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    vk_user_id: Mapped[int] = mapped_column(nullable=False)
+    name: Mapped[str] = mapped_column(String[50])
+    game_id: Mapped[int] = mapped_column(ForeignKey("games.id"))
+
+    game: Mapped["Game"] = relationship(back_populates="players")
+    player_answers_games: Mapped[list["PlayerAnswerGame"]] = relationship(
+        back_populates="player", cascade="all, delete-orphan"
+    )
+
+
+class PlayerAnswerGame(BaseModel):
+    __tablename__ = "player_answers_games"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    player_id: Mapped[int] = mapped_column(ForeignKey("players.id"))
+    game_id: Mapped[int] = mapped_column(ForeignKey("games.id"))
+    answer_id: Mapped[int] = mapped_column(ForeignKey("answers.id"))
+
+    player: Mapped[list["Player"]] = relationship(
+        back_populates="player_answers_games"
+    )
+    game: Mapped["Game"] = relationship(back_populates="player_answers_games")
+    answer: Mapped["Answer"] = relationship(
+        back_populates="player_answers_games"
+    )
--- app/store/database/__init__.py
+++ app/store/database/__init__.py
@@ -1,1 +1,3 @@
 from app.admin.models import *
+from app.game.models import *
+from app.quize.models import *
