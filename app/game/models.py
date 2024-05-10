97c8cb16eb069995fe6e71613a6742f7e6bdf312
Safonov <Sofik.d@gmail.com>
2024-05-10 19:26:24 +0300

    Импортировал Enum из диалектов

--- app/game/models.py
+++ app/game/models.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
-from sqlalchemy import Enum as PgEnum, ForeignKey, String
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.dialects.postgresql import ENUM as PgEnum
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.quize.models import Answer, Question
@@ -25,7 +26,7 @@ class Game(BaseModel):
     )
     question_id: Mapped[int] = mapped_column(ForeignKey("questions.id"))
     game_stage: Mapped[PgEnum] = mapped_column(
-        PgEnum(GameStage), default=GameStage.NOT_START
+        PgEnum(GameStage), default=GameStage.NOT_START.value, nullable=False
     )
 
     question: Mapped["Question"] = relationship(back_populates="games")
