44b0b89479599dc29f79d80a6ce17f27b3bbad8c
Safonov <Sofik.d@gmail.com>
2024-05-14 00:37:23 +0300

    рефакторинг

--- app/store/game/constants.py
+++ app/store/game/constants.py
@@ -4,20 +4,20 @@ from app.store.vk_api.utils import VkButton
 
 
 class VkButtons(Enum):
-    BTN_STOP_GAME = VkButton(label="Стоп игра", color="primary")
+    BTN_STOP_GAME = VkButton(label="Стоп игра", color="primary").get()
     BTN_REG_ON = VkButton(
         label="Буду играть",
         type_btn="callback",
-        payload={"type": "show_snackbar", "text": "REG_ON"},
+        payload={"type": "show_snackbar", "text": "/reg_on"},
         color="secondary",
-    )
+    ).get()
     BTN_REG_OFF = VkButton(
         label="Отменить регистрацию",
         type_btn="callback",
-        payload={"type": "show_snackbar", "text": "REG_OFF"},
-    )
+        payload={"type": "show_snackbar", "text": "/reg_off"},
+    ).get()
     BTN_ANSWER = VkButton(
         label="Знаю ответ!",
         type_btn="callback",
-        payload={"type": "show_snackbar", "text": "Give answer"},
-    )
+        payload={"type": "show_snackbar", "text": "/give_answer"},
+    ).get()
