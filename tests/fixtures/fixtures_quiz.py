d93163c7bde893e9265607b142dca19922c8fb92
Safonov <Sofik.d@gmail.com>
2024-05-19 23:13:18 +0300

    Создание вопросов

---
+++ tests/fixtures/fixtures_quiz.py
@@ -0,0 +1,82 @@
+import pytest
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
+
+from app.quiz.models import (
+    Answer,
+    Question,
+    Theme,
+)
+
+
+@pytest.fixture
+async def theme_1(
+    db_sessionmaker: async_sessionmaker[AsyncSession],
+) -> Theme:
+    new_theme = Theme(title="default_test")
+
+    async with db_sessionmaker() as session:
+        session.add(new_theme)
+        await session.commit()
+
+    return new_theme
+
+
+@pytest.fixture
+async def theme_2(
+    db_sessionmaker: async_sessionmaker[AsyncSession],
+) -> Theme:
+    new_theme = Theme(title="theme2_test")
+
+    async with db_sessionmaker() as session:
+        session.add(new_theme)
+        await session.commit()
+
+    return new_theme
+
+
+@pytest.fixture
+async def question_1(
+    db_sessionmaker: async_sessionmaker[AsyncSession], theme_1: Theme
+) -> Question:
+    answers = [
+        Answer(title="Еду", score=43),
+        Answer(title="свадьбу", score=27),
+        Answer(title="природу", score=15),
+        Answer(title="животных", score=15),
+    ]
+
+    question = Question(
+        title="Кого или что чаще всего снимает фотограф?",
+        theme_id=1,
+        answers=answers,
+    )
+
+    async with db_sessionmaker() as session:
+        session.add(question)
+        await session.commit()
+
+    return question
+
+
+@pytest.fixture
+async def question_2(db_sessionmaker, theme_1: Theme) -> Question:
+    answers = [
+        Answer(title="Москва", score=34),
+        Answer(title="санкт-петербург", score=17),
+        Answer(title="новосибирск", score=14),
+        Answer(title="казань", score=12),
+        Answer(title="екатеринбург", score=8),
+        Answer(title="Самара", score=5),
+    ]
+
+    question = Question(
+        title="В каком городе России есть метро?",
+        theme_id=1,
+        answers=answers,
+    )
+
+    async with db_sessionmaker() as session:
+        session.add(question)
+        await session.commit()
+
+    return question
--- tests/quiz/test_views/test_quiz.py
+++ tests/quiz/test_views/test_quiz.py
@@ -1,5 +1,7 @@
 from aiohttp.test_utils import TestClient
 
+from app.quiz.models import Question
+
 
 class TestQuestionAddView:
     async def test_unauthorized(self, cli: TestClient) -> None:
@@ -9,6 +11,100 @@ class TestQuestionAddView:
 
         data = await response.json()
         assert data["status"] == "unauthorized"
+
+    async def test_success(
+        self, auth_cli: TestClient, question_1: Question
+    ) -> None:
+        response = await auth_cli.post(
+            "/quiz.questions_add",
+            json={
+                "title": "Что в тяжелой коробке. что в ней?",
+                "theme_id": 1,
+                "answers": [
+                    {"id": 0, "title": "Еду", "score": 10},
+                    {"id": 0, "title": "холодильник", "score": 20},
+                    {"id": 0, "title": "доллары", "score": 30},
+                    {"id": 0, "title": "телевизор", "score": 40},
+                ],
+            },
+        )
+        assert response.status == 200
+
+        data = await response.json()
+        assert data == {
+            "status": "ok",
+            "data": {
+                "id": 2,
+                "title": "Что в тяжелой коробке. что в ней?",
+                "theme_id": question_1.theme_id,
+                "answers": [
+                    {"id": 5, "title": "Еду", "score": 10},
+                    {"id": 6, "title": "холодильник", "score": 20},
+                    {"id": 7, "title": "доллары", "score": 30},
+                    {"id": 8, "title": "телевизор", "score": 40},
+                ],
+            },
+        }
+
+    async def test_bad_score(
+        self, auth_cli: TestClient, question_1: Question
+    ) -> None:
+        response = await auth_cli.post(
+            "/quiz.questions_add",
+            json={
+                "id": 0,
+                "title": "два человека тащут тяжелую коробку. что в ней?",
+                "theme_id": 1,
+                "answers": [
+                    {"id": 0, "title": "Еду", "score": 25},
+                    {"id": 0, "title": "холодильник", "score": 25},
+                    {"id": 0, "title": "доллары", "score": 25},
+                    {"id": 0, "title": "телевизор", "score": 26},
+                ],
+            },
+        )
+        assert response.status == 400, response.json()
+
+        data = await response.json()
+        assert (
+            data.get("message")
+            == "Сумма очков всех ответов должна быть равна 100, текущая сумма: 101"
+        )
+
+    async def test_empty_title(
+        self, auth_cli: TestClient, question_1: Question
+    ) -> None:
+        response = await auth_cli.post(
+            "/quiz.questions_add",
+            json={
+                "id": 0,
+                "title": "",
+                "theme_id": 1,
+                "answers": [
+                    {"id": 0, "title": "Еду", "score": 25},
+                    {"id": 0, "title": "холодильник", "score": 25},
+                    {"id": 0, "title": "доллары", "score": 25},
+                    {"id": 0, "title": "телевизор", "score": 26},
+                ],
+            },
+        )
+        assert response.status == 400, response.json()
+
+    async def test_empty_answers(
+        self, auth_cli: TestClient, question_1: Question
+    ) -> None:
+        response = await auth_cli.post(
+            "/quiz.questions_add",
+            json={
+                "id": 0,
+                "title": "ЧТо в ящике?",
+                "theme_id": 1,
+                "answers": [
+                    {"id": 0, "title": "Еду", "score": 50},
+                ],
+            },
+        )
+        assert response.status == 400, response.json()
 
 
 class TestQuestionDeleteByIdView:
