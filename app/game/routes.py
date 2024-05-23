88264f323bdeb18d358ed1da4703b5b82339cc26
Dmitry <42798721+Safonovdv91@users.noreply.github.com>
2024-05-23 15:23:02 +0300

    Tests and bughunt (#20)
    * hotfix очепятка
    
    * Удалил не используемые модули
    
    * реф
    
    * реф
    
    * поменял пароль для боевого сервера
    
    * Отключил не нужные роуты
    
    * Покрыт тестами роут с настройками игры
    
    ---------
    
    Co-authored-by: Safonov <Sofik.d@gmail.com>

--- app/game/routes.py
+++ app/game/routes.py
@@ -5,8 +5,6 @@ from app.game.views import (
     GameGetByIdView,
     GameListView,
     GameProfileListActiveView,
-    PatchSettingsView,
-    SettingsAddView,
     SettingsGetByIdView,
 )
 
@@ -20,6 +18,6 @@ def setup_routes(app: "Application"):
     app.router.add_view("/game.get_by_id", GameGetByIdView)
 
     app.router.add_view("/game/profile.get_by_id", SettingsGetByIdView)
-    app.router.add_view("/game/profile.add", SettingsAddView)
-    app.router.add_view("/game/profile.patch", PatchSettingsView)
+    # app.router.add_view("/game/profile.patch", SettingsAddView)
+    # app.router.add_view("/game/profile.patch", PatchSettingsView)
     app.router.add_view("/game/profile.default", DefaultSettingsView)
