e81a7938d1f634b257dfac273d769138aa3540a3
Safonov <Sofik.d@gmail.com>
2024-05-23 14:11:31 +0300

    реф

--- tests/quiz/test_views/test_game.py
+++ tests/quiz/test_views/test_game.py
@@ -37,7 +37,7 @@ class TestListActive:
                             "description": None,
                             "id": 1,
                             "max_count_gamers": 6,
-                            "min_count_gamers": 1,
+                            "min_count_gamers": 2,
                             "profile_name": "test",
                             "time_to_answer": 15,
                             "time_to_registration": 30,
@@ -63,7 +63,7 @@ class TestListActive:
         }
 
 
-class TestGetById:
+class TestGetGameById:
     async def test_unauthorized(self, cli: TestClient) -> None:
         response = await cli.get("/game.get_by_id", params={"game_id": 1})
 
@@ -89,7 +89,7 @@ class TestGetById:
                     "description": None,
                     "id": 1,
                     "max_count_gamers": 6,
-                    "min_count_gamers": 1,
+                    "min_count_gamers": 2,
                     "profile_name": "test",
                     "time_to_answer": 15,
                     "time_to_registration": 30,
@@ -136,3 +136,155 @@ class TestGetById:
             "message": "Unprocessable Entity",
             "status": "bad_request",
         }
+
+
+class TestGameSettingsGetById:
+    async def test_unauthorized(self, cli: TestClient) -> None:
+        response = await cli.get(
+            "game/profile.get_by_id", params={"profile_id": 1}
+        )
+
+        assert response.status == 401, f"response = {response}"
+
+        data = await response.json()
+        assert data["status"] == "unauthorized"
+
+    async def test_success(self, auth_cli: TestClient, game_settings) -> None:
+        response = await auth_cli.get(
+            "game/profile.get_by_id", params={"profile_id": 1}
+        )
+
+        assert response.status == 200, f"response = {response}"
+
+        data = await response.json()
+        assert data == {
+            "data": {
+                "description": None,
+                "id": 1,
+                "max_count_gamers": 6,
+                "min_count_gamers": 2,
+                "profile_name": "test",
+                "time_to_answer": 15,
+                "time_to_registration": 30,
+            },
+            "status": "ok",
+        }
+
+    async def test_success_bad_id(
+        self, auth_cli: TestClient, game_settings
+    ) -> None:
+        response = await auth_cli.get(
+            "game/profile.get_by_id", params={"profile_id": "sd"}
+        )
+        assert response.status == 400, f"response = {response}"
+        data = await response.json()
+
+        assert data == {
+            "data": {"querystring": {"profile_id": ["Not a valid integer."]}},
+            "message": "Unprocessable Entity",
+            "status": "bad_request",
+        }
+
+
+class TestChangeProfile:
+    async def test_unauthorized(self, cli: TestClient) -> None:
+        response = await cli.get(
+            "game/profile.default", params={"time_to_registration": 12}
+        )
+
+        assert response.status == 401, f"response = {response}"
+
+        data = await response.json()
+        assert data["status"] == "unauthorized"
+
+    async def test_success(self, auth_cli: TestClient, game_settings) -> None:
+        response = await auth_cli.patch(
+            "game/profile.default", params={"time_to_registration": 122}
+        )
+
+        assert response.status == 200, f"response = {response}"
+
+        data = await response.json()
+        assert data == {"data": {}, "status": "ok"}
+
+        response = await auth_cli.get(
+            "game/profile.get_by_id", params={"profile_id": 1}
+        )
+        assert response.status == 200
+
+        data = await response.json()
+        assert (
+            data
+            == {
+                "data": {
+                    "description": None,
+                    "id": 1,
+                    "max_count_gamers": 6,
+                    "min_count_gamers": 2,
+                    "profile_name": "test",
+                    "time_to_answer": 15,
+                    "time_to_registration": 122,
+                },
+                "status": "ok",
+            }
+            != {"data": {}, "status": "ok"}
+        )
+
+    async def test_success_min_max(
+        self, auth_cli: TestClient, game_settings
+    ) -> None:
+        response = await auth_cli.patch(
+            "game/profile.default",
+            params={
+                "min_count_gamers": 3,
+                "max_count_gamers": 7,
+            },
+        )
+
+        assert response.status == 400, f"response = {response}"
+
+        data = await response.json()
+        assert data == {"data": {}, "status": "ok"}
+
+        response = await auth_cli.get(
+            "game/profile.get_by_id", params={"profile_id": 1}
+        )
+        assert response.status == 200
+
+        data = await response.json()
+        assert data == {
+            "data": {
+                "description": None,
+                "id": 1,
+                "max_count_gamers": 7,
+                "min_count_gamers": 3,
+                "profile_name": "test",
+                "time_to_answer": 15,
+                "time_to_registration": 30,
+            },
+            "status": "ok",
+        }
+
+    async def test_success_bad_max(
+        self, auth_cli: TestClient, game_settings
+    ) -> None:
+        response = await auth_cli.patch(
+            "game/profile.default",
+            params={
+                "max_count_gamers": 1,
+            },
+        )
+
+        assert response.status == 400, f"response = {response}"
+
+        data = await response.json()
+        assert (
+            data
+            == {
+                "data": {},
+                "message": "Максимальное кол-во игроков должно"
+                " быть больше минимального(2)",
+                "status": "bad_request",
+            }
+            != {"data": {}, "status": "ok"}
+        )
