cbf6731bb642fc0ff9f3b9dc97199bb1233e88b3
Dmitry <42798721+Safonovdv91@users.noreply.github.com>
2024-12-17 15:56:02 +0300

    Fix cors problem (#27)
    * REWORK: Переделана логика передачи заголовков CORS: удалена библиотека aiohttp_cors, обработка заголовков вынесена в middleware.
    
    * REWORK: Переделана логика передачи заголовков CORS: удалена библиотека aiohttp_cors, обработка заголовков вынесена в middleware.
    
    * REWORK: Переделана логика передачи заголовков CORS: удалена библиотека aiohttp_cors, обработка заголовков вынесена в middleware.
    
    * REFACTORING: ruff refactoring
    
    * REF: выделены адреса с origins
    
    ---------
    
    Co-authored-by: Safonov <Sofik.d@gmail.com>

--- app/web/config.py
+++ app/web/config.py
@@ -43,14 +43,17 @@ class Config:
 
 
 def setup_config(app: "Application", config_path: str):
+    # Сюда добавляются адресы откуда будет идти запрос на api
+    # т.е. Адреса с реактом админки
     allowed_origins = [
         "http://0.0.0.0:3000",
-        "http://0.0.0.0:3001",
         "http://localhost:3000",
-        "http://localhost:3001",
-        "http://localhost:4545",
-        "http://0.0.0.0:4545",
     ]
+    # добавление адресов с локальной разработкой
+    for i in range(10):
+        allowed_origins.append(f"http://10.252.1.{i}:3000")
+        allowed_origins.append(f"http://10.252.1.{i}:3001")
+        
     with open(config_path, "r") as f:
         raw_config = yaml.safe_load(f)
 
