dfe1eb9a521ae9c8f7647015257492fd017b6d9f
Safonov <Sofik.d@gmail.com>
2024-12-23 16:12:20 +0300

    ADD: Добавлен DEV статус разработки

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM python:3.12-slim
 
+ARG APP_VERSION= 1.0.1
+MAINTAINER Safonov
+
+LABEL version=$APP_VERSION
+EXPOSE 8000
+
 WORKDIR /app
 
 COPY requirements.txt .
@@ -7,3 +13,6 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
+RUN chmod +x start.sh
+CMD ["python", "./main.py"]
+
--- app/web/config.py
+++ app/web/config.py
@@ -1,10 +1,14 @@
 import typing
 from dataclasses import dataclass
+from logging import getLogger
+from os import getenv
 
 import yaml
 
 if typing.TYPE_CHECKING:
     from app.web.app import Application
+
+logger = getLogger(__name__)
 
 
 @dataclass
@@ -42,33 +46,58 @@ class Config:
     allowed_origins: list[str] | None = None
 
 
-def setup_config(app: "Application", config_path: str):
-    # Сюда добавляются адресы откуда будет идти запрос на api
-    # т.е. Адреса с реактом админки
-    allowed_origins = [
-        "http://0.0.0.0:3000",
-        "http://localhost:3000",
-    ]
-    # добавление адресов с локальной разработкой
-    for i in range(10):
-        allowed_origins.append(f"http://10.252.1.{i}:3000")
-        allowed_origins.append(f"http://10.252.1.{i}:3001")
+def setup_config(app: "Application", config_path: str) -> None:
+    if getenv("STATUS") == "DEV":
+        logger.info("Dev mode activated")
+        allowed_origins = [
+            "http://0.0.0.0:3000",
+            "http://localhost:3000",
+            "http://localhost:3001",
+        ]
+        # добавление адресов с локальной разработкой
+        for i in range(10):
+            allowed_origins.append(f"http://10.252.1.{i}:3000")
+            allowed_origins.append(f"http://10.252.1.{i}:3001")
 
-    with open(config_path, "r") as f:
-        raw_config = yaml.safe_load(f)
+        logger.info("Allowed origins: %s", allowed_origins)
 
-    app.config = Config(
-        session=SessionConfig(
-            key=raw_config["session"]["key"],
-        ),
-        admin=AdminConfig(
-            email=raw_config["admin"]["email"],
-            password=raw_config["admin"]["password"],
-        ),
-        bot=BotConfig(
-            token=raw_config["bot"]["token"],
-            group_id=raw_config["bot"]["group_id"],
-        ),
-        database=DatabaseConfig(**raw_config["database"]),
-        allowed_origins=allowed_origins,
-    )
+        app.config = Config(
+            session=SessionConfig(
+                key="-6b97CSkbEPmpKWCBixxy9yER2IXEyoO19XFj420dKs=",
+            ),
+            admin=AdminConfig(
+                email="admin@admin.com",
+                password="admin",
+            ),
+            bot=BotConfig(
+                token="vk1.a.XIDKJZTUyQ24nViGDAYxN78v2VvAXtu1In2ra9rI9i9Gx6L0KoKS2GFcrjFTfpr71wvatzW9N8_LMwpuZxU5Gi-TYMxE6ybdIZ4NBZeUYRFntpiKUvHPDwVmx4QTCWCAN9rEuVJwhzKAh8BbYM6xZf6hdLoi5nyY5WBoaeaXsd6yfWFZKpzBi5N6zIfmAYFUoTNgjvgRc-hQbTovpHBI6A",
+                group_id=218897004,
+            ),
+            database=DatabaseConfig(
+                host=getenv("DB_HOST"),
+                port=int(getenv("DB_PORT")),
+                user=getenv("DB_USER"),
+                password=getenv("DB_PASSWORD"),
+                database=getenv("DB_NAME"),
+            ),
+            allowed_origins=allowed_origins,
+        )
+    else:
+        with open(config_path, "r") as f:
+            raw_config = yaml.safe_load(f)
+
+        app.config = Config(
+            session=SessionConfig(
+                key=raw_config["session"]["key"],
+            ),
+            admin=AdminConfig(
+                email=raw_config["admin"]["email"],
+                password=raw_config["admin"]["password"],
+            ),
+            bot=BotConfig(
+                token=raw_config["bot"]["token"],
+                group_id=raw_config["bot"]["group_id"],
+            ),
+            database=DatabaseConfig(**raw_config["database"]),
+            allowed_origins=allowed_origins,
+        )
