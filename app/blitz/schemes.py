48a39f107d4765e830b1871ff9a92b366fc72b65
Safonov <Sofik.d@gmail.com>
2024-12-24 16:03:27 +0300

    ADD: Добавлены скемы с новым именем, чтобы не было проблем

--- app/blitz/schemes.py
+++ app/blitz/schemes.py
@@ -1,46 +1,42 @@
 from marshmallow import Schema, fields, validate
 
 
-class ThemeSchema(Schema):
+class BlitzThemeSchema(Schema):
     id = fields.Int(required=False)
     title = fields.Str(required=True)
     description = fields.Str(required=False)
 
 
-class QuestionSchema(Schema):
+class BlitzQuestionSchema(Schema):
     id = fields.Int(required=False)
     title = fields.Str(required=True)
     theme_id = fields.Int(required=True)
     answer = fields.Str(required=True)
 
 
-class ThemeListQuerySchema(Schema):
+class BlitzThemeListQuerySchema(Schema):
     limit = fields.Int(required=False, validate=validate.Range(min=1))
     offset = fields.Int(required=False, validate=validate.Range(min=1))
 
 
-class ThemeListSchema(Schema):
-    themes = fields.Nested(ThemeSchema, many=True)
-
-
-class ThemeIdSchema(Schema):
+class BlitzThemeIdSchema(Schema):
     theme_id = fields.Int(required=True, validate=validate.Range(min=1))
 
 
-class ThemeQueryIdSchema(ThemeIdSchema):
+class BlitzThemeQueryIdSchema(BlitzThemeIdSchema):
     offset = fields.Int(required=False, validate=validate.Range(min=1))
     limit = fields.Int(required=False, validate=validate.Range(min=1))
 
 
-class QuestionIdSchema(Schema):
+class BlitzQuestionIdSchema(Schema):
     question_id = fields.Int(validate=validate.Range(min=1))
 
 
-class QuestionListSchema(Schema):
-    questions = fields.Nested(QuestionSchema, many=True)
+class BlitzQuestionListSchema(Schema):
+    questions = fields.Nested(BlitzQuestionSchema, many=True)
 
 
-class QuestionPatchRequestsSchema(Schema):
+class BlitzQuestionPatchRequestsSchema(Schema):
     title = fields.Str(required=False)
     theme_id = fields.Int(required=False)
     # answers = fields.Nested(
