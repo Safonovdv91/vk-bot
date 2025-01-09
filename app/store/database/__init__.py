7acea4374aed1785fcd505a7148cfe65b45f900a
Safonov <Sofik.d@gmail.com>
2025-01-09 15:18:35 +0300

    bugfix: добавил модели для алмбика

--- app/store/database/__init__.py
+++ app/store/database/__init__.py
@@ -2,3 +2,5 @@ from app.admin.models import *
 from app.blitz.models import *
 from app.games.blitz.models import *
 from app.games.game_100.models import *
+from app.quiz.models import *
+from app.vk.models import *
